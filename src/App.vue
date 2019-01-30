5682980cb76f7529e81bcfa06260241d795d9782
MalkovVladimir <vovchik123123@yandex.ru>
2019-01-30 16:33:34 +0300

    template created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <air-schedule />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AirSchedule from './components/AirSchedule.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    AirSchedule
   }
 }
 </script>
---
+++ src/components/AirSchedule.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="schedule">
+        {{ schedule }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: "AirSchedule",
+  data() {
+    return {
+      schedule: null,
+      IsDataLoaded: false
+    };
+  },
+  created: function() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/json");
+
+    fetch(
+      
+      {
+        method: "GET",
+        headers: myHeaders,
+        mode: "cors",
+        cache: "default"
+      }
+    ).then(resp => {
+      this.schedule = resp.json();
+    });
+  }
+};
+</script>
+
+<style>
+</style>
