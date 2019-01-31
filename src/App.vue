90c53c21384cf6b3180e724ef92a5ad81ebfc761
MalkovVladimir <vovchik123123@yandex.ru>
2019-01-31 16:23:42 +0300

    SVO schedule release
    Schdeule based on fake data

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,20 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <air-schedule />
+    <air-schedule 
+      v-if="isDataReady" 
+      :flights="flights"
+      :airports="airports"
+    />
   </div>
 </template>
 
 <script>
+/**
+ * SVO - код аэропорта шереметьево
+ */
+import SVO_Schedule from './fakeData/SVO_Schedule.json'
+import SVO_Statuses from './fakeData/SVO_Statuses.json'
 import HelloWorld from './components/HelloWorld.vue'
 import AirSchedule from './components/AirSchedule.vue'
 
@@ -15,6 +24,54 @@ export default {
   components: {
     HelloWorld,
     AirSchedule
+  },
+  data() {
+    return {
+      flights: {},
+      airports: {},
+      isDataReady: false
+    }
+  },
+  created: function() {
+    this.parseRawData(this.getFakeSVOData())
+  },
+  methods: {
+    getSVOData: function() {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "text/json");
+
+      fetch(
+        "https://api.flightstats.com/flex/schedules/rest/v1/json/to/SVO/arriving/2019/1/31/9?appId=4e6e22ed&appKey=84e0e5775586e5b18c5de421ffe0fa3e",
+        {
+          method: "GET",
+          headers: myHeaders,
+          mode: "cors",
+          cache: "default",
+          qs: {
+            appId: "4e6e22ed",
+            appKey: "84e0e5775586e5b18c5de421ffe0fa3e",
+            arrivalAirportCode: "SVO",
+            year: new Date().getYear(),
+            month: new Date().getMonth(),
+            day: new Date().getDay(),
+            hourOfDay: new Date().getHours()
+          }
+        }
+      ).then(resp => {
+        this.schedule = resp.json();
+      });
+    },
+    getFakeSVOData: function() {
+      return SVO_Schedule
+    },
+    parseRawData: function(data) {
+      data.appendix.airports.forEach(airport => {
+        const {fs, ...clean} = airport
+        this.airports[airport.fs] = clean
+      });
+      this.flights = data.scheduledFlights
+      this.isDataReady = true
+    }
   }
 }
 </script>
--- src/components/AirSchedule.vue
+++ src/components/AirSchedule.vue
@@ -1,36 +1,98 @@
 <template>
-    <div v-if="schedule">
-        {{ schedule }}
-    </div>
+  <div class="timetable-wrap">
+    <table class="timetable">
+      <thead>
+        <tr>
+          <th class="timetable__cell">Номер рейса</th>
+          <th class="timetable__cell">Город</th>
+          <!--<th class="timetable__cell">arrivalAirport</th>-->
+          <th class="timetable__cell">Терминал</th>
+          <th class="timetable__cell">Время прибытия</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr 
+          v-for="flight in flights"
+          :key="flight.carrierFsCode + flight.flightNumber"
+          class="timetable__row"
+        >
+          <td class="timetable__cell">
+            {{ flight.flightNumber }}
+          </td>
+          <td class="timetable__cell">
+            <div>
+              {{ airports[flight.departureAirportFsCode].city }}
+            </div>
+            <div>
+              {{ airports[flight.departureAirportFsCode].countryName }}
+            </div>
+          </td>
+          <!--<td class="timetable__cell">
+            <div>
+              {{ airports[flight.arrivalAirportFsCode].city }}
+            </div>
+            <div>
+              {{ airports[flight.arrivalAirportFsCode].countryName }}
+            </div>
+          </td>-->
+          <td class="timetable__cell">
+            {{ flight.arrivalTerminal }}
+          </td>
+          <td class="timetable__cell">
+            {{ flight.arrivalTime | wrapAsNotification }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AirSchedule",
-  data() {
-    return {
-      schedule: null,
-      IsDataLoaded: false
-    };
-  },
-  created: function() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "text/json");
+  filters: {
+    wrapAsNotification: function(rawDateStr) {
+      var date = new Date(rawDateStr);
+      var minutes = date.getMinutes();
+      minutes = minutes > 9 ? minutes : '0' + minutes;
 
-    fetch(
-      
-      {
-        method: "GET",
-        headers: myHeaders,
-        mode: "cors",
-        cache: "default"
-      }
-    ).then(resp => {
-      this.schedule = resp.json();
-    });
+      if (date > Date.now())
+        return `Ожидается в ${date.getHours()}:${minutes}`
+      else
+        return `Совершил посадку в ${date.getHours()}:${minutes}`
+    }
+  },
+  props: {
+    flights: {
+      type: Array,
+      required: true
+    },
+    airports: {
+      type: Object,
+      required: true
+    }
   }
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.timetable-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.timetable {
+  border-collapse: collapse;
+  box-shadow: 0 2px 4px 0 rgba(134, 121, 71, 0.45);
+}
+.timetable__row {
+  border-top: 1px solid lightgray;
+
+  &:hover {
+    background-color: whitesmoke;
+  }
+}
+.timetable__cell {
+  padding: 1rem;
+}
 </style>
